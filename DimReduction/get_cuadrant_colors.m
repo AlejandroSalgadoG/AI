1f9500e85b5611b687e73ccd265817143691fa13
Alejandro Salgado G <asalgad2@eafit.edu.co>
2018-09-28 11:54:55 -0500

    Adding second optimization to Lle

--- DimReduction/get_cuadrant_colors.m
+++ DimReduction/get_cuadrant_colors.m
@@ -1,8 +1,16 @@
-function colors = get_cuadrant_colors(x, z, org)
+function colors = get_cuadrant_colors(x, z)
     n = size(x,1);
-    colors = 1:n;
-    colors(x >= org & z > org) = 1;
-    colors(x < org & z >= org) = 2;
-    colors(x <= org & z < org) = 3;
-    colors(x > org & z <= org) = 4;
+    colors = zeros(n,1);
+    
+    m = 0.85;
+    pt = [12.5 0];
+    
+    % cuadrant 1
+    colors(x >= 0 & z > 0) = 1;    
+    colors(z >= -m*(x-pt(1))) = 2;
+    % cuadrant 2
+    colors(x > 0 & z <= 0) = 3;
+    colors(z < m*(x-pt(1))) = 4;
+    % cuadrant 4
+    colors(x <= 0 & z < 0 ) = 5;
 end
