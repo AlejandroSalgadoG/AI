8250076a291226b56c553019ead136d66acd647b
Alejandro Salgado G <asalgad2@eafit.edu.co>
2018-11-04 19:06:52 -0500

    Adding implementation section to Lle documentation

--- DimReduction/Lle/Lle.m
+++ DimReduction/Lle/Lle.m
@@ -1,5 +1,5 @@
 rng(10); 
-colordef none;
+colordef white;
 
 n = 1000;
 [x, y, z] = build_swiss_roll(n);
@@ -14,19 +14,17 @@ p = 2;
 tol=0.001;
 
 W = zeros(n);
+e = ones(k,1);
 for i=1:n
     x_i = data(i,:);
     neighbors = k_neighbors(data, x_i, k);
-
     v_i = data(neighbors,:)';
-    e = ones(k,1);
     x_e = repmat(x_i, k, 1)';
     g = (x_e - v_i)' * (x_e - v_i);
     g = g + eye(k)*tol*trace(g); % regularization
     w_i = g\e;
     w_i = w_i / sum(w_i);
     %norm( v_i*w_i - x_i')
-    
     W(neighbors,i) = w_i;
 end
 
@@ -38,5 +36,4 @@ M = (I-W) * (I-W)';
 [eigvec, ~] = eigs(M,p+1,'SM');
 Y = eigvec(:,2:p+1);
 
-plot_swiss_roll(Y(:,1),Y(:,1),zeros(n,1), colors, [-15 15])
 plot_swiss_roll(Y(:,1),Y(:,1),zeros(n,1), colors, [-0.1 0.1])
