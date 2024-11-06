7ddedc78bd8226db448abc4f52c69fe1b3b89e09
Alejandro Salgado <alejando.salgado@benchling.com>
2024-11-06 12:06:34 -0500

    add output parser to linkeding chain

--- GenAI/langchain/ice_breaker.py
+++ GenAI/langchain/ice_breaker.py
@@ -3,18 +3,25 @@ from langchain_ollama import ChatOllama
 
 from linkedin_scraper import scrape_linkedin_profile
 from linkedin_agent import linkedin_lookup_agent
+from output_parser import summary_parser
 
 if __name__ == "__main__":
     summary_template = """
     given the Linkedin information {information} about a person I wany you to create:
         1. short summary
         2. two interesting facts about them
+
+    {format_instructions}
     """
 
-    prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+    prompt_template = PromptTemplate(
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={"format_instructions": summary_parser.get_format_instructions()},
+    )
     llm = ChatOllama(model="llama3.1")
 
-    chain = prompt_template | llm
+    chain = prompt_template | llm | summary_parser
 
     linkedin_url = linkedin_lookup_agent(name="Eden Marco Udemy")
     linkedin_data = scrape_linkedin_profile(linkedin_url)
