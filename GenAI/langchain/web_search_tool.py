b02db4dd943d1f590e36eb14315c81478c63ddbf
Alejandro Salgado <alejando.salgado@benchling.com>
2024-11-06 10:21:22 -0500

    add mock output to web search tool

--- GenAI/langchain/web_search_tool.py
+++ GenAI/langchain/web_search_tool.py
@@ -1,6 +1,24 @@
 from langchain_community.tools.tavily_search import TavilySearchResults
 
 
-def get_profile_url_tavily(name: str) -> dict[str, str]:
+def get_profile_url_tavily(name: str, mock: bool = True) -> dict[str, str]:
+    if mock:
+        return {
+            'query': name,
+            'follow_up_questions': None,
+            'answer': None,
+            'images': None,
+            'results': [
+                {
+                    'title': 'Eden Marco Udemy - linkedin',
+                    'url': '"https://www.linkedin.com/in/eden-marco"',
+                    'content': '',
+                    'score': 0.98567,
+                    'raw_content': None
+                }
+            ],
+            'response_time': 1.07
+        }
+
     search = TavilySearchResults()
     return search.run(name)
