e8d679316616fc53667cc81984eb6b520b38fdbd
Alejandro Salgado <salgadoalejandro1996@gmail.com>
2025-05-22 15:03:24 -0500

    add images to populate db script in rag

--- GenAI/rag-img-summary/populate_db.py
+++ GenAI/rag-img-summary/populate_db.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser, ArgumentTypeError
 from datetime import date, datetime, timedelta
 
+from image_handler import ImageHandler
 from storage import Storage
 from scraper import iter_dates, get_articles_url, WebLoader
 from text_handler import TextHandler
@@ -53,6 +54,7 @@ if __name__ == "__main__":
 
     storage = Storage()
     text_handler = TextHandler()
+    image_handler = ImageHandler()
     dates = iter_dates(
         start_date=args.start_date,
         delta=timedelta(days=7),
@@ -63,5 +65,6 @@ if __name__ == "__main__":
         if articles_url := get_articles_url(d):
             loader = WebLoader(web_paths=articles_url)
             for web_data in loader.lazy_load_web_paths():
-                docs = text_handler.apply(web_data)
-                storage.add_documents(docs)
+                text_docs = text_handler.apply(web_data)
+                image_docs = image_handler.apply(web_data)
+                storage.store(text_docs + image_docs)
