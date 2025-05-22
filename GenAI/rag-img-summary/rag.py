41b3886bb7c3a0e1712f48f94d1d7ed2486ac740
Alejandro Salgado <salgadoalejandro1996@gmail.com>
2025-05-22 14:51:43 -0500

    copy rag as rag-img-summary

---
+++ GenAI/rag-img-summary/rag.py
@@ -0,0 +1,60 @@
+from langchain_core.documents import Document
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from langchain_ollama import ChatOllama
+from langchain.prompts import ChatPromptTemplate
+
+from config import configs
+from storage import Storage
+
+
+class Rag:
+    def __init__(self):
+        self.config = configs["llm"]
+        self.llm = ChatOllama(
+            model=self.config.model,
+            **self.config.model_params,
+        )
+        self.last_context = []
+        self.retriever = Storage().get_vector_store_retriver()
+        self.chain = (
+            {
+                "context": RunnableLambda(self.get_context),
+                "question": RunnablePassthrough(),
+            }
+            | ChatPromptTemplate.from_template(self.config.prompt)
+            | self.llm
+            | StrOutputParser()
+        )
+
+    def get_context(self, query: str) -> str:
+        documents = self.retriever.invoke(query)
+        self.last_context = [document.page_content for document in documents]
+        return self.parse_retrived(documents)
+
+    def parse_retrived(self, data: list[Document]) -> str:
+        return "\n\n".join([d.page_content for d in data])
+
+    def consult(self, query: str) -> str:
+        return self.chain.invoke(query)
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+
+    arg_parser = ArgumentParser(
+        prog="rag.py",
+        description="script to execute multimodal rag example",
+    )
+    arg_parser.add_argument(
+        "query",
+        help="query to execute",
+        nargs="?",
+        type=str,
+        default="what do you know about microsoft phi-4-reasoning?",
+    )
+
+    args = arg_parser.parse_args()
+
+    result = Rag().consult(args.query)
+    print(result)
---
+++ GenAI/rag-img-summary/test_dataset.py
@@ -0,0 +1,30 @@
+golden_data = [
+    {
+        "question": "What is phi-4-reasoning?",
+        "expected_answer": "Phi-4-reasoning is a fine-tuned version of a pretrained model. It is as part of Microsoft's latest recipe for training reasoning models",
+    },
+    {
+        "question": "Are EU regulations on AI getting stricter or relaxed?",
+        "expected_answer": "EU regulations on AI are getting relaxed. The European Union has made an abrupt U-turn away from its stringent AI regulations",
+    },
+    {
+        "question": "Is it worth it to speed up AI tasks?",
+        "expected_answer": "Yes. Making certain tasks much faster, whether or not they also become cheaper, can create significant new value for businesses.",
+    },
+    {
+        "question": "What are memory layers used for?",
+        "expected_answer": "Memory layers are used to compute vectors that may capture details like names or dates, which were learned through training, and retrieve them according to a given input.",
+    },
+    {
+        "question": "When should I use DeepCoder?",
+        "expected_answer": "When you need to develop or improve coding models using reinforcement learning",
+    },
+    {
+        "question": "Attention mechanisms are better than memory layers?",
+        "expected_answer": "attention mechanisms and memory layers have different functionalities but are not directly compared in terms of which is better.",
+    },
+    {
+        "question": "Can EU citizens sue AI companies for damages caused by their systems?",
+        "expected_answer": "existing laws already allow consumers to file claims against AI companies in some cases. However, the EU's AI Act offers a comprehensive framework that aims to regulate AI more specifically.",
+    },
+]
